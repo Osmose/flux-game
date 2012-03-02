1eb9eae9046ace8b3e141b3febf7924e5f5a54a6
James Long <jlongster@jlongster.com>
2012-03-01 17:23:41 -0800

    bullets shouldn't shoot through walls

--- js/bullet.js
+++ js/bullet.js
@@ -5,20 +5,31 @@ define(function(require) {
 
     function Bullet(engine, x, y, speed, dir) {
         Entity.call(this, engine);
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-        this.dir = dir;
+        return _.extend(this, {
+            x: x,
+            y: y,
+            speed: speed,
+            dir: dir,
+            bounding_box: {left:0, top:0, right:5, bottom:5}
+        });
     }
 
     _.extend(Bullet.prototype, Entity.prototype, {
         tick: function() {
             this.x += this.speed * (this.dir == util.LEFT ? -1 : 1);
+
+            var c = this.engine.collides({left: this.x + this.bounding_box.left,
+                                          right: this.x + this.bounding_box.right,
+                                          top: this.y + this.bounding_box.top,
+                                          bottom: this.y + this.bounding_box.bottom});
+            if(c.solid) {
+                return false;
+            }
         },
         
         render: function(ctx, x, y) {
             ctx.fillStyle = 'green';
-            ctx.fillRect(this.x - x, this.y - y, 2, 2);
+            ctx.fillRect(this.x - x, this.y - y, 5, 5);
         }
     });
 
